0a49dd7507922ce05a8e7a0ce45b6fe0d94912fd
Tanvirshahriargit <tanvirshahriar20188@gmail.com>
2021-10-29 20:35:57 +0600

    post and get api added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5000;
 const { MongoClient } = require('mongodb');
 //  dot env require
 require('dotenv').config();
@@ -14,8 +15,6 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l5n9e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -25,7 +24,24 @@ async function run(req, res) {
         await client.connect();
         console.log("connected db");
         const database = client.db("travelGuru");
-        const usersCollection = database.collection("users");
+      const servicesCollection = database.collection("services");
+      
+      // POST API 
+      app.post('/services', async (req, res) => {
+        const services = req.body;
+        console.log("hitted cliend", services);
+        const result = await servicesCollection.insertOne(services);
+        console.log('Got new user', req.body);
+        console.log("added services", result);
+        res.send(result);
+      });
+
+      // GET API 
+      app.get('/services', async (req, res) => {
+        const allServices = servicesCollection.find({})
+        const result = await allServices.toArray();
+        res.send(result)
+      })
     }
     finally {
         // await client.close();
